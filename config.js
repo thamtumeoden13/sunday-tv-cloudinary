f0409fe34420d40497609ee8a8b28a8fdebf7bf7
Le Hoang Vu <ThamTuMeoDen13@Les-MacBook-Pro.local>
2019-11-29 14:15:30 +0700

    4

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 exports.CLIENT_ORIGIN = process.env.NODE_ENV === 'development'
-  ? 'https://sunday-tv-cloudinary.herokuapp.com'
+  ? 'https://sunday-tv-cloudinary-v2.herokuapp.com'
   : 'http://localhost:8080'
